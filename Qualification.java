ede4aba282e69ce4d77fbc4dfc00983e8352af8e
abhishekkorishetty <145687043+abhishekkorishetty@users.noreply.github.com>
2024-08-06 16:53:10 +0530

    adding file

---
+++ Qualification.java
@@ -0,0 +1,13 @@
+public class Qualification {
+    public static void main(String[] args) {
+        String name="abhishek";
+        String qualification="mca";
+        int percentage=83;
+        if(qualification=="mca"){
+            System.out.println("it is matched:"+qualification);
+
+        }else{
+            System.out.println("not matched:"+qualification);
+        }
+    }
+}
